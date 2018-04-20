0bf99eb230e5981e51e0cdc159a5b2dbb99ce14c
jorgemi <38523693+jorgemi@users.noreply.github.com>
2018-04-20 13:42:13 +0200

    Se modifica controlLogin

--- controlador/controlLoguin.php
+++ controlador/controlLoguin.php
@@ -1,21 +1,30 @@
 <?php 
 	//ini_set("session.use_cookie",0);
 	//session_cache_limiter("nocache");
-	if(isset($_POST['user']) && !empty($_POST['user'])){
+	echo 'hola';
+	if(isset($_POST['nif']) && !empty($_POST['password'])){
+		echo 'hola';
 		require_once('../modelo/modeloLogin.php');
-		$user=$_POST['user'];
-		$pwd=$_POST['pwd'];
+		$nif=$_POST['nif'];
+		$pwd=$_POST['password'];
 		$pwd= md5($pwd);
-		$usuario=consultaUser($user,$pwd);
+		$usuario=consultaUser($nif,$pwd);
+		print_r ($usuario);
 		if($usuario!= false){
+			
 			//$_SESSION
-			session_start();
+			//session_start();
 			print_r($usuario);
 			print_r($_SESSION);
+			if ($usuario['rol']==1){
+
+				header("Location: ../vista/user.php");
+				
 			}
-		
-		
-		
-	
+		}
+		else{
+			header('Location: ../vista/badlogin.php');
+			exit;
+		}
 	}
 ?>
